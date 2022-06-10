a48c09fd04673e00bcd737135f20b0d07fa61a67
LeticiaGuimas <leticia_guimaraes2025@hotmail.com>
2022-06-10 16:14:23 -0300

    first commit

---
+++ src/components/Card.jsx
@@ -0,0 +1,7 @@
+import Card from '../../node_modules/@material-ui/core/Card/Card'
+import CardContent from '../../node_modules/@material-ui/core/CardContent/CardContent'
+
+export {
+  Card,
+  CardContent
+}
